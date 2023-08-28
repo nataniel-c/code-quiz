87b8dfa0a86add44a0ec7f6dbde0c1cf5fd7e4a1
Nataniel Carrasquillo <nataniel-carras@hotmail.com>
2023-08-28 18:30:36 -0500

    added basic code for css html and js

---
+++ assets/script.js
@@ -0,0 +1,140 @@
+
+// Create a list of questions with premade answers
+const questions = [
+    { question: "Question 1", 
+        answers: [
+            { text: "A", correct: "" },
+            { text: "B", correct: "" },
+            { text: "C", correct: "" },
+            { text: "D", correct: "" },
+        ]
+    },
+    { question: "", 
+        answers: [
+            { text: "", correct: "" },
+            { text: "", correct: "" },
+            { text: "", correct: "" },
+            { text: "", correct: "" },
+        ]
+    },
+    { question: "", 
+        answers: [
+            { text: "", correct: "" },
+            { text: "", correct: "" },
+            { text: "", correct: "" },
+            { text: "", correct: "" },
+        ]
+    },
+    { question: "", 
+        answers: [
+            { text: "", correct: "" },
+            { text: "", correct: "" },
+            { text: "", correct: "" },
+            { text: "", correct: "" },
+        ]
+    },
+    { question: "", 
+        answers: [
+            { text: "", correct: "" },
+            { text: "", correct: "" },
+            { text: "", correct: "" },
+            { text: "", correct: "" },
+        ]
+    },
+    { question: "", 
+        answers: [
+            { text: "", correct: "" },
+            { text: "", correct: "" },
+            { text: "", correct: "" },
+            { text: "", correct: "" },
+        ]
+    },
+    { question: "", 
+        answers: [
+            { text: "", correct: "" },
+            { text: "", correct: "" },
+            { text: "", correct: "" },
+            { text: "", correct: "" },
+        ]
+    },
+    { question: "", 
+        answers: [
+            { text: "", correct: "" },
+            { text: "", correct: "" },
+            { text: "", correct: "" },
+            { text: "", correct: "" },
+        ]
+    },
+    { question: "", 
+        answers: [
+            { text: "", correct: "" },
+            { text: "", correct: "" },
+            { text: "", correct: "" },
+            { text: "", correct: "" },
+        ]
+    },
+    { question: "", 
+        answers: [
+            { text: "", correct: "" },
+            { text: "", correct: "" },
+            { text: "", correct: "" },
+            { text: "", correct: "" },
+        ]
+    }    
+];
+
+// Reference the elements that will be changed in the HTML as the quiz progresses
+
+// Elements outside of the quiz
+// Introduction
+const introElement = document.getElementById("intro");
+
+// Elements within the quiz
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answers");
+const nextButton = document.getElementById("next-btn");
+const button1 = [document.getElementById("answer1"), 
+    document.getElementById("answer2"), 
+    document.getElementById("answer3"), 
+    document.getElementById("answer4")];
+
+var currentQuestionIndex = 0;
+var currentScore = 0;
+var scoreHistory = [];
+var time = 0;
+
+function introduceQuiz() {
+    currentQuestionIndex = 0;
+    currentScore = 0;
+    time = 0;
+    nextButton.innerHTML = "Start Quiz";
+    nextButton.addEventListener("click", askQuestion);
+    questionElement.innerHTML = "Coding Quiz Challenge";
+    answerButton.style.display = "none";
+    var running = 1;
+    console.log(running);
+    
+}
+
+function askQuestion() {
+
+    nextButton.innerHTML = "Next Question";
+    answerButton.style.display = "";
+    var currentQuestion = questions[currentQuestionIndex];
+    var questionNumber = currentQuestionIndex++ + 1;
+    questionElement.innerHTML = questionNumber + ". " + currentQuestion.question;
+    button1.innerHTML = currentQuestion.answers[0].text;
+    button2.innerHTML = currentQuestion.answers[1].text;
+    button3.innerHTML = currentQuestion.answers[2].text;
+    button4.innerHTML = currentQuestion.answers[3].text;
+    button1.addEventListener("click", evaluateAns);
+
+
+}
+
+function evaluateAns() {
+    if 
+}
+
+
+introduceQuiz();
